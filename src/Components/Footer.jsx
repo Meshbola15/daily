ab7ef9d052aa140465a9246f93500d49b8fc129c
Meshbola15 <Meshbola15@gmail.com>
2023-01-18 23:14:59 +0100

    Fuck off

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,36 +2,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" w-full flex flex-col px-12 py-12 lg:px-28 lg:py-12 bg-white">
-      <div className="relative flex items-center justify-between">
+    <div className=" w-full flex flex-col px-10 py-10 lg:px-28 lg:py-12 bg-white">
+      <div className="relative flex flex-col lg:flex-row items-start lg:items-center lg:justify-between">
         <div className="text-black flex flex-col">
-          <p className="text-2xl font-bold">Contacts</p>
+          <p className="text-3xl font-bold">Contacts</p>
           <a href="mailto:info@usedaily.io">info@usedaily.io</a>
         </div>
         <a
           href="http://"
           target="_blank"
           rel="noopener noreferrer"
-          className="align-middle self-center bg-black text-white px-[20px] py-[10px] rounded-[15px] flex items-center justify-center"
+          className="align-middle self-start font-medium my-4 lg:my-0 lg:self-center bg-black text-white px-[20px] py-[10px] rounded-[15px] flex items-center justify-center  relative lg:top-10 "
         >
-          Join the waitlist
+          Join the Waitlist
         </a>
 
         <div className="flex flex-col text-right">
-          <p className="text-2xl font-bold">Daily</p>
+          <p className="text-3xl font-bold">Daily</p>
         </div>
       </div>
 
-      <div className=" flex flex-row items-center justify-between">
+      <div className=" flex flex-col lg:flex-row items-start lg:items-center md:justify-between lg:mt-5">
         <a
           href="#"
           target="_blank"
           rel="noopener noreferrer"
-          className="mt-7 font-bold text-md "
+          className="mt-7 font-medium text-md "
         >
           Terms of Use & Privacy Policy
         </a>
-        <a href="" className="font-bold text-md">
+        <a href="" className="font-medium text-md">
           usedaily.io
         </a>
       </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Iphone from "../Assets/iphone.svg";
+import Iphone from "../Assets/iphone.png";
 import App from "../Assets/Daily_card.svg";
 import Card from "../Assets/Card.svg";
 import bitcoin from "../Assets/Bitcoin.png";
@@ -9,15 +9,17 @@ import creditCard from "../Assets/Magnetic.png";
 import Usd from "../Assets/Us.png";
 
 const Hero = () => {
-  const tabHeaderStyles = "text-2xl text-bold";
-  const tabParaghapStyles = "text-md";
+  const tabHeaderStyles = "text-2xl lg:text-3xl font-[500]";
+  const tabParaghapStyles = "text-md lg:text-lg";
 
   return (
-    <div className="w-full min-h-screen gradient-background px-12 py-12 lg:px-28 lg:py-5">
-      <h2 className="text-white text-[40px] font-bold mb-24">Daily</h2>
-      <div className="flex items-center justify-between flex-col lg:flex-row">
-        <div className="text-white text-center lg:w-1/2 lg:text-left w-full">
-          <h2 className=" text-3xl lg:text-5xl font-bold">
+    <div className="w-full min-h-screen gradient-background px-10 py-10 lg:px-28 lg:py-5">
+      <h2 className="text-white  text-[25px] lg:text-[40px] font-bold mb-5">
+        Daily
+      </h2>
+      <div className="flex items-center justify-between flex-col lg:flex-row w-full">
+        <div className="text-white text-center mt-0 lg:-mt-[35vh] mr-0 lg:mr-10 lg:w-[50vw] lg:text-left w-full">
+          <h2 className=" text-4xl mb-4 lg:text-6xl font-bold">
             Easy Crypto Spending
           </h2>
           <p className="text-md lg:text-lg mt-1 mb-8">
@@ -28,21 +30,21 @@ const Hero = () => {
             href="#"
             target="_blank"
             rel="noopener noreferrer"
-            className="px-[20px] py-[10px] bg-white text-black rounded-[15px] text-md font-normal"
+            className="px-[20px] py-[10px] bg-white text-black font-medium rounded-[15px] text-md"
           >
-            Join the waitlist
+            Join the Waitlist
           </a>
         </div>
         <img
           src={Iphone}
           alt="Image"
-          className="w-full mt-12 h-auto md:w-[60vw] lg:h-[85vh] lg:w-auto lg:mt-0"
+          className=" mt-12 h-auto w-full lg:h-[120vh] lg:w-auto lg:mt-0 mr-0 pr-0"
         />
-        <img src="" alt="" class />
+        {/* <img src="" alt="" class /> */}
       </div>
-      <div className="flex flex-col items-center my-[120px] text-center lg:text-left lg:my-0 lg:min-h-screen lg:justify-between lg:flex-row">
-        <div className="text-white w-full lg:w-[45vw]">
-          <h2 className="text-3xl font-bold mb-5">Daily</h2>
+      <div className="flex flex-col items-center justify-center my-[120px] text-center lg:text-left lg:my-0 lg:min-h-screen lg:justify-between lg:flex-row">
+        <div className="text-white w-full h-auto lg:w-[45vw]">
+          <h2 className="text-5xl font-bold mb-5">Daily</h2>
           <p className="text-md">
             "Daily connects your custodial and non-custodial wallets, allowing
             you to spend your cryptocurrencies anytime, anywhere, instantly
@@ -50,39 +52,55 @@ const Hero = () => {
             while earning $DAILY rewards."
           </p>
         </div>
-        <img src={Card} alt="Daily_card" className="mt-20" />
+        <img src={Card} alt="Daily_card" className="lg:mt-0 mt-20" />
       </div>
 
       <div className="my-20 lg:min-h-screen flex items-center flex-col lg:flex-row justify-between">
-        <div className="flex items-start justify-start flex-col">
-          <div className="items-center flex flex-row justify-start mb-1">
-            <img src={bitcoinExchange} alt="Bitcoin / usd exchange" />
-            <section className="flex flex-col text-white ml-10">
+        <div className="flex items-start w-full lg:w-1/2 justify-start flex-col">
+          <div className="items-start flex flex-row justify-start mb-4">
+            <img
+              src={bitcoinExchange}
+              alt="Bitcoin / usd exchange"
+              className="w-[40px] lg:w-auto"
+            />
+            <section className="flex flex-col text-white ml-4 lg:ml-10">
               <h2 className={tabHeaderStyles}>Exchanges and Wallets</h2>
               <p className={tabParaghapStyles}>
                 Connect your desired Crypto Exchange and Wallet accounts
               </p>
             </section>
           </div>
-          <div className="items-center flex flex-row justify-start mb-1">
-            <img src={bitcoin} alt="Bitcoin / usd exchange" />
-            <section className="flex flex-col text-white ml-10">
+          <div className="items-start flex flex-row justify-start mb-4">
+            <img
+              src={bitcoin}
+              alt="Bitcoin / usd exchange"
+              className="w-[40px] lg:w-auto"
+            />
+            <section className="flex flex-col text-white ml-4 lg:ml-10">
               <h2 className={tabHeaderStyles}>Cryptocurrencies</h2>
               <p className={tabParaghapStyles}>
                 Spend more than +200 cryptocurrencies
               </p>
             </section>
           </div>
-          <div className="items-center flex flex-row justify-start mb-1">
-            <img src={star} alt="Bitcoin / usd exchange" />
-            <section className="flex flex-col text-white ml-10">
+          <div className="items-start flex flex-row justify-start mb-4">
+            <img
+              src={star}
+              alt="Bitcoin / usd exchange"
+              className="w-[40px] lg:w-auto"
+            />
+            <section className="flex flex-col text-white ml-4 lg:ml-10">
               <h2 className={tabHeaderStyles}>Anonymity</h2>
               <p className={tabParaghapStyles}>No KYC</p>
             </section>
           </div>
-          <div className="items-center flex flex-row justify-start mb-1">
-            <img src={creditCard} alt="Bitcoin / usd exchange" />
-            <section className="flex flex-col text-white ml-10">
+          <div className="items-start flex flex-row justify-start mb-4">
+            <img
+              src={creditCard}
+              alt="Bitcoin / usd exchange"
+              className="w-[40px] lg:w-auto"
+            />
+            <section className="flex flex-col text-white ml-4 lg:ml-10">
               <h2 className={tabHeaderStyles}>Virtual debitcard</h2>
               <p className={tabParaghapStyles}>
                 Connect your Daily card to Apple Pay or Google Pay and
@@ -90,9 +108,13 @@ const Hero = () => {
               </p>
             </section>
           </div>
-          <div className="items-center flex flex-row justify-start mb-1">
-            <img src={Usd} alt="Bitcoin / usd exchange" />
-            <section className="flex flex-col text-white ml-10">
+          <div className="items-start flex flex-row justify-start mb-4">
+            <img
+              src={Usd}
+              alt="Bitcoin / usd exchange"
+              className="w-[40px] lg:w-auto"
+            />
+            <section className="flex flex-col text-white ml-4 lg:ml-10">
               <h2 className={tabHeaderStyles}>Use daily</h2>
               <p className={tabParaghapStyles}>
                 Pay with crypto anywhere VISA is accepted
@@ -103,7 +125,7 @@ const Hero = () => {
         <img
           src={App}
           alt="App"
-          className="w-3/4 mt-10 lg:mt-0 md:w-[50%] ml-0  lg:h-[90vh] lg:w-auto"
+          className="w-full mt-10 lg:mt-0 md:w-[50vw] ml-0 lg:h-[110vh] lg:w-auto"
         />
       </div>
     </div>
