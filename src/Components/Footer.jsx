327462202acfe1b4ccc4ef54fa1b38bd2806badd
Meshbola15 <Meshbola15@gmail.com>
2023-01-21 23:07:01 +0100

    fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" w-full flex flex-col px-10 py-10 lg:px-28 lg:py-12 bg-white">
+    <div className=" w-full flex flex-col px-10 py-10 lg:px-12 lg:py-12 bg-white">
       <div className="relative flex flex-col lg:flex-row items-start lg:items-center lg:justify-between">
         <div className="text-black flex flex-col">
           <p className="text-3xl font-bold">Contacts</p>
-          <a href="mailto:info@usedaily.io">info@usedaily.io</a>
+          <a href="mailto:info@usedaily.ioun">info@usedaily.ioun</a>
         </div>
         <a
           href="http://"
           target="_blank"
           rel="noopener noreferrer"
-          className="align-middle self-start font-medium my-4 lg:my-0 lg:self-center bg-black text-white px-[20px] py-[10px] rounded-[15px] flex items-center justify-center  relative lg:top-10 "
+          className="align-middle self-start font-medium my-4 -ml-0 lg:-ml-8 lg:my-0 lg:self-center  bg-black text-white px-[30px] py-[15px] rounded-[15px] flex items-center justify-center  lg:relative lg:top-10 "
         >
           Join the Waitlist
         </a>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,19 +10,19 @@ import Usd from "../Assets/Us.png";
 
 const Hero = () => {
   const tabHeaderStyles = "text-2xl lg:text-3xl font-[500]";
-  const tabParaghapStyles = "text-md lg:text-lg";
+  const tabParaghapStyles = "text-neutral-400 text-lg lg:text-xl";
 
   return (
-    <div className="w-full min-h-screen gradient-background px-10 py-10 lg:px-28 lg:py-5">
-      <h2 className="text-white  text-[25px] lg:text-[40px] font-bold mb-5">
+    <div className="w-full min-h-screen gradient-background px-10 py-10 lg:px-12 lg:py-5">
+      <h2 className="text-white text-[25px] lg:text-[40px] font-bold mb-20 lg:mb-5">
         Daily
       </h2>
       <div className="flex items-center justify-between flex-col lg:flex-row w-full">
-        <div className="text-white text-center mt-0 lg:-mt-[35vh] mr-0 lg:mr-10 lg:w-[50vw] lg:text-left w-full">
+        <div className="text-white text-center mt-0 mr-0 lg:mr-10 lg:w-[50vw] lg:text-left w-full">
           <h2 className=" text-4xl mb-4 lg:text-6xl font-bold">
             Easy Crypto Spending
           </h2>
-          <p className="text-md lg:text-lg mt-1 mb-8">
+          <p className="text-base text-neutral-400 lg:text-xl mt-1 mb-8">
             Select Exchange or Wallet. Choose cryptocurrency. Pay everywhere
             with one app and no physical payment card.
           </p>
@@ -30,7 +30,7 @@ const Hero = () => {
             href="#"
             target="_blank"
             rel="noopener noreferrer"
-            className="px-[20px] py-[10px] bg-white text-black font-medium rounded-[15px] text-md"
+            className="px-[30px] py-[15px] bg-white text-black font-medium rounded-[15px] text-md"
           >
             Join the Waitlist
           </a>
@@ -38,14 +38,14 @@ const Hero = () => {
         <img
           src={Iphone}
           alt="Image"
-          className=" mt-12 h-auto w-full lg:h-[120vh] lg:w-auto lg:mt-0 mr-0 pr-0"
+          className=" mt-12 h-auto w-full lg:-mt-24 lg:w-auto lg:h-screen mr-0 pr-0"
         />
         {/* <img src="" alt="" class /> */}
       </div>
       <div className="flex flex-col items-center justify-center my-[120px] text-center lg:text-left lg:my-0 lg:min-h-screen lg:justify-between lg:flex-row">
         <div className="text-white w-full h-auto lg:w-[45vw]">
           <h2 className="text-5xl font-bold mb-5">Daily</h2>
-          <p className="text-md">
+          <p className="text-lg lg:text-xl text-neutral-400">
             "Daily connects your custodial and non-custodial wallets, allowing
             you to spend your cryptocurrencies anytime, anywhere, instantly
             using Apple Pay or Google Pay without identity verification, all
@@ -57,7 +57,7 @@ const Hero = () => {
 
       <div className="my-20 lg:min-h-screen flex items-center flex-col lg:flex-row justify-between">
         <div className="flex items-start w-full lg:w-1/2 justify-start flex-col">
-          <div className="items-start flex flex-row justify-start mb-4">
+          <div className="items-center flex flex-row justify-start mb-4">
             <img
               src={bitcoinExchange}
               alt="Bitcoin / usd exchange"
@@ -70,7 +70,7 @@ const Hero = () => {
               </p>
             </section>
           </div>
-          <div className="items-start flex flex-row justify-start mb-4">
+          <div className="items-center flex flex-row justify-start mb-4">
             <img
               src={bitcoin}
               alt="Bitcoin / usd exchange"
@@ -83,7 +83,7 @@ const Hero = () => {
               </p>
             </section>
           </div>
-          <div className="items-start flex flex-row justify-start mb-4">
+          <div className="items-center flex flex-row justify-start mb-4">
             <img
               src={star}
               alt="Bitcoin / usd exchange"
@@ -94,7 +94,7 @@ const Hero = () => {
               <p className={tabParaghapStyles}>No KYC</p>
             </section>
           </div>
-          <div className="items-start flex flex-row justify-start mb-4">
+          <div className="items-center flex flex-row justify-start mb-4">
             <img
               src={creditCard}
               alt="Bitcoin / usd exchange"
@@ -108,7 +108,7 @@ const Hero = () => {
               </p>
             </section>
           </div>
-          <div className="items-start flex flex-row justify-start mb-4">
+          <div className="items-center flex flex-row justify-start mb-4">
             <img
               src={Usd}
               alt="Bitcoin / usd exchange"
@@ -125,7 +125,7 @@ const Hero = () => {
         <img
           src={App}
           alt="App"
-          className="w-full mt-10 lg:mt-0 md:w-[50vw] ml-0 lg:h-[110vh] lg:w-auto"
+          className="w-full mt-10 lg:mt-0 md:w-[50vw] ml-0 lg:h-[100vh] lg:w-auto"
         />
       </div>
     </div>
